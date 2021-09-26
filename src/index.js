4c7bf6aece3dd51ebe6842ac4ed527f851f83a20
EdisonFlorez <eflorezb@unal.edu.co>
2021-09-26 14:50:53 -0500

    installation completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const server = express();
 const morgan = require('morgan');
+const cors = require('cors')
 // configuraciones
 server.set('port', process.env.PORT || 3000);
 // middlewares
 server.use(morgan('tiny'));
+server.use(cors());
 server.use(express.json()); 
 server.use(express.urlencoded({extended : true}));
+// rutas 
+
+// middleware Vue
+const history = require('connect-history-api-fallback');
+server.use(history());
 
 // configura la carpeta a la que va a acceder.
 server.use(express.static(__dirname + '/public'));
