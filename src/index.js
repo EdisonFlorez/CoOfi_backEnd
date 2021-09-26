3d6f977a6fb25d192e850c69f07a33cca137148e
EdisonFlorez <eflorezb@unal.edu.co>
2021-09-26 11:35:35 -0500

    Se inicio el servidor

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+// configuraciones
+server.set('port', process.env.PORT || 3000);
+// middlewares
+server.use(morgan('tiny'));
+server.use(express.json()); 
+server.use(express.urlencoded({extended : true}));
+
+// configura la carpeta a la que va a acceder.
+server.use(express.static(__dirname + '/public'));
+
+// configuración del puerto donde está escuchando
+server.listen(server.get('port'), () => {
+    console.log("funciona");
+} );
