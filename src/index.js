c0930f3b10019063bddb53f9c042183093128361
EdisonFlorez <eflorezb@unal.edu.co>
2021-09-27 21:56:39 -0500

    configuracion babel mongoose

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-const express = require('express');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
 const server = express();
-const morgan = require('morgan');
-const cors = require('cors')
-// configuraciones
+
+// configuracionesn del puerto
 server.set('port', process.env.PORT || 3000);
+
+// conexión base de datos
+
 // middlewares
 server.use(morgan('tiny'));
 server.use(cors());
 server.use(express.json()); 
 server.use(express.urlencoded({extended : true}));
+
 // rutas 
 
 // middleware Vue
@@ -20,7 +26,7 @@ server.use(express.static(__dirname + '/public'));
 
 // configuración del puerto donde está escuchando
 server.listen(server.get('port'), () => {
-    console.log("funciona");
+    console.log("Funciona en el puerto " + server.get('port'));
 } );
 
 
